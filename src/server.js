d78c49c78ef2c292127a1723622ee830896b09dd
jiseonBaek <js.coding.place@gmail.com>
2025-01-28 16:39:16 +0900

    chore: set another server port

--- src/server.js
+++ src/server.js
@@ -1,6 +1,21 @@
 const app = require('./app');
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+const startServer = (port) => {
+  const server = app.listen(port, () => {
+    console.log(`Server running on port ${port}`);
+  });
+
+  server.on('error', (err) => {
+    if (err.code === 'EADDRINUSE') {
+      console.log(
+        `Port ${port} is in use. Trying another port...`
+      );
+      startServer(port + 1);
+    } else {
+      console.error(err);
+    }
+  });
+};
+
+startServer(PORT);
